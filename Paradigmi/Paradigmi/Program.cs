47032d0c72055ee8b13746bbbc16048c54bbbd07
Andrea-DallAra <andrea.dallara@studenti.unicam.it>
2024-06-19 17:01:27 +0200

    prova

--- Paradigmi/Paradigmi/Program.cs
+++ Paradigmi/Paradigmi/Program.cs
@@ -11,6 +11,7 @@ namespace Paradigmi
 {
     public class Program
     {
+        //prova
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
